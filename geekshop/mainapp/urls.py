86940a8fdfe6955906404e919b3ce3f35cc1edec
GBAleksandrGB <tomilov.alex@gmail.com>
2022-04-17 21:54:15 +0300

    tasks solving, lesson3

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from .views import main, products, contact
 
+from .views import products
+
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('', main, name='main'),
-    path('products/', products, name='products'),
-    path('contact/', contact, name='contact'),
-    path('products_all/', products, name='products_all'),
-    path('products_home/', products, name='products_home'),
-    path('products_office/', products, name='products_office'),
-    path('products_modern/', products, name='products_modern'),
-    path('products_classic/', products, name='products_classic'),
+    path('', products, name='index'),
+    path('<int:pk>/', products, name='category'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,36 +1,49 @@
 from django.shortcuts import render
 
-links_menu = [
-    {'href': 'main', 'name': 'Домой'},
-    {'href': 'products', 'name': 'Продукты'},
-    {'href': 'contact', 'name': 'Контакты'},
-]
-
-content_links_menu = {
-    'links_menu': links_menu,
-}
-
-links_products = [
-    {'href': 'products_all', 'name': 'все'},
-    {'href': 'products_home', 'name': 'дом'},
-    {'href': 'products_office', 'name': 'офис'},
-    {'href': 'products_modern', 'name': 'модерн'},
-    {'href': 'products_classic', 'name': 'классика'},
-]
-
-content_links_products = {
-    'links_products': links_products,
-    'links_menu': links_menu,
-}
+from mainapp.models import Product
 
 
 def main(request):
-    return render(request, 'mainapp/index.html', context=content_links_menu)
+    title = 'Главная'
+    links_menu = [
+        {'href': 'main', 'name': 'Домой'},
+        {'href': 'products', 'name': 'Продукты'},
+        {'href': 'contact', 'name': 'Контакты'},
+    ]
+    products = Product.objects.all()[:4]
+    content = {
+        'title': title,
+        'products': products,
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    return render(request, 'mainapp/products.html', context=content_links_products)
+def products(request, pk=None):
+    title = 'Каталог'
+    links_menu = [
+        {'href': 'main', 'name': 'Домой'},
+        {'href': 'products', 'name': 'Продукты'},
+        {'href': 'contact', 'name': 'Контакты'},
+    ]
+    links_products = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    content = {'title': title, 'links_menu': links_menu, 'links_products': links_products}
+    print(pk)
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html', context=content_links_menu)
+    title = 'Контакты'
+    links_menu = [
+        {'href': 'main', 'name': 'Домой'},
+        {'href': 'products', 'name': 'Продукты'},
+        {'href': 'contact', 'name': 'Контакты'},
+    ]
+    content = {'title': title, 'links_menu': links_menu}
+    return render(request, 'mainapp/contact.html', content)
