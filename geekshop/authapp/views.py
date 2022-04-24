e7c8e4c097d95402ea0abe624186957004786c73
GBAleksandrGB <tomilov.alex@gmail.com>
2022-04-24 23:08:03 +0300

    tasks solving, lesson6

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -8,16 +8,28 @@ from django.urls import reverse
 
 def login(request):
     title = 'Вход'
-    login_form = ShopUserLoginForm(data=request.POST)
+    login_form = ShopUserLoginForm(data=request.POST or None)
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
+
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(username=username, password=password)
+
         if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('main'))
 
-    content = {'title': title, 'login_form': login_form}
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('main'))
+
+    content = {
+        'title': title,
+        'login_form': login_form,
+        'next': next,
+    }
+
     return render(request, 'authapp/login.html', content)
 
 
@@ -35,7 +47,11 @@ def register(request):
             return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
-    content = {'title': title, 'register_form': register_form}
+    content = {
+        'title': title,
+        'register_form': register_form,
+    }
+
     return render(request, 'authapp/register.html', content)
 
 
@@ -49,5 +65,9 @@ def edit(request):
     else:
         edit_form = ShopUserEditForm(instance=request.user)
 
-    content = {'title': title, 'edit_form': edit_form}
+    content = {
+        'title': title,
+        'edit_form': edit_form,
+    }
+
     return render(request, 'authapp/edit.html', content)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Product, ProductCategory
 from basketapp.models import Basket
+from random import sample
 
 
 def main(request):
@@ -20,6 +21,23 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
+
 def products(request, pk=None):
     title = 'Каталог'
     links_menu = [
@@ -29,7 +47,7 @@ def products(request, pk=None):
     ]
     links_products = ProductCategory.objects.all()
 
-    basket = []
+    basket = get_basket(request.user)
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
 
@@ -51,14 +69,37 @@ def products(request, pk=None):
         }
         return render(request, 'mainapp/products_list.html', content)
 
-    same_products = Product.objects.all()[:]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
     content = {
         'title': title,
         'links_menu': links_menu,
         'links_products': links_products,
-        'same_products': same_products
+        'same_products': same_products,
+        'hot_product': hot_product,
+        'basket': basket,
     }
     return render(request, 'mainapp/products.html', content)
+
+
+def product(request, pk):
+    title = 'Продукты'
+    links_menu = [
+        {'href': 'main', 'name': 'Домой'},
+        {'href': 'products', 'name': 'Продукты'},
+        {'href': 'contact', 'name': 'Контакты'},
+    ]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'links_products': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', content)
 
 
 def contact(request):
